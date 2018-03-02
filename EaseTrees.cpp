553c8187b23f04b114f1b4fe79f3f59404fb01a1
ishu9bansal <ishubansal1400@gmail.com>
2018-03-02 15:41:01 +0530

    Update EaseTrees.cpp
    Changes related to addition of square vector
    Future addition of header files in single header file

--- EaseTrees.cpp
+++ EaseTrees.cpp
@@ -1,7 +1,7 @@
 // EaseTrees.cpp : Defines the entry point for the console application.
 //
 
-#include "Node.h"
+#include "Header.h"
 #include <iostream>
 
 using namespace std;
@@ -10,6 +10,6 @@ int main()
 {
 	cout << "Hello World" << endl;
 	testNode();
+	testSquareVector();
     return 0;
 }
-
